a45abc85a489348cb56086a2f9626cbaeaeebcd1
Fellipe Castro <contact@fellipecastro.com>
2016-01-02 13:04:39 -0200

    dfd

--- url_shortener/shorturls/views.py
+++ url_shortener/shorturls/views.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.base import RedirectView
